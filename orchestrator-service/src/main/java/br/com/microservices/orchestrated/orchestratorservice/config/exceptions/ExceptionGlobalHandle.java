8c16e1cce952c30dd8661d97eba8aec1015ff6ca
Edvaldo S de Matos - avanade <e.santiago.de.matos@avanade.com>
2024-11-17 16:28:00 -0300

    Criacao das Exceptions

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/config/exceptions/ExceptionGlobalHandle.java
@@ -0,0 +1,16 @@
+package br.com.microservices.orchestrated.orchestratorservice.config.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionGlobalHandle {
+
+	@ExceptionHandler(ValidationException.class)
+	public ResponseEntity<ExceptionDetails> handleValidationException(ValidationException ex) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDetails(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
+	}
+}
